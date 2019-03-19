0c4cb92a33e1a7d31f31b4896e6d72e4f737d2e5
denis909 <denis909@mail.ru>
2019-03-19 14:25:38 +0300

    n/a

--- CascadeFilesystem.php
+++ CascadeFilesystem.php
@@ -1,95 +1,15 @@
 <?php
 /**
- * @copyright Copyright (c) 2018 denis909
- * @license https://github.com/denis909/yii2-cascade-filesystem/blob/master/LICENSE
+ * @copyright Copyright (c) denis909 2018
+ * @license MIT
  * @author denis909
  * @link http://denis909.spb.ru
  */
-
 namespace denis909\yii;
 
-use Yii;
-use yii\helpers\ArrayHelper;
-
-class CascadeFilesystem extends \yii\base\Component
+class CascadeFilesystem extends BaseCascadeFilesystem
 {
 
-	public $pathMap = [];
-
-	public function init()
-	{
-		parent::init();
-
-		spl_autoload_register([$this, 'autoload'], true, false);
-	}
-
-	public function autoload($class)
-	{
-		foreach($this->pathMap as $fromNamespace => $toNamespace)
-		{
-			$segments = explode("\\", $class);
-
-			$className = array_pop($segments);
-
-			$classNamespace = implode("\\", $segments);
-
-			$classNamespaceAlias = '@' . str_replace("\\", '/', $classNamespace);
-
-			if ($classNamespaceAlias == $toNamespace)
-			{
-				$filename = Yii::getAlias($fromNamespace) . '/' . $className . '.php';
-
-				if (is_file($filename))
-				{
-					require_once $filename;
-
-					$exists = class_exists($class, false) || interface_exists($class, false) || trait_exists($class, false);	
-				
-					if ($exists)
-					{
-						return true;
-					}
-				}				
-			}
-		}
-
-		return false;
-	}
-
-	public static function collect(string $file, array $default = [], $aliases = null)
-	{
-		$filename = __DIR__ . '/' . $file . '.php';
-
-		if (is_file($filename))
-		{
-			$return = require $filename;
-		}
-		else
-		{
-			$return = [];
-		}
-
-		if ($aliases === null)
-		{
-			$aliases =  require Yii::getAlias('@common/config') . '/modules.php'; 
-		}
-
-		foreach($aliases as $key => $alias)
-		{
-			$filename = Yii::getAlias($alias) .'/' . $file . '.php';
-
-			if (is_file($filename))
-			{
-		    	$return = ArrayHelper::merge($return, require $filename);
-			}
-		}
-
-		if ($default)
-		{
-			return ArrayHelper::merge($return, $default);
-		}
-
-		return $return;
-	}
+    public $configFile = '@common/config/modules';
 
 }
