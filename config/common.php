5b4bd307d120c11dee4ce6dee81dc5027239e609
denis909 <denis909@mail.ru>
2020-09-27 17:50:43 +0300

    n/a

--- config/common.php
+++ config/common.php
@@ -7,7 +7,7 @@
 return [
     'bootstrap' => [
         'cascadeFilesystem'
-    ]
+    ],
     'components' => [
         'cascadeFilesystem' => [
             'class' => 'denis909\yii\CascadeFilesystem'
